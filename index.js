d7d711e7a2c8bc3b6e4dbf47a6dcc3502a9538a4
rituraj <riturajroj1234@gmail.com>
2022-05-02 16:12:37 +0530

    main

---
+++ index.js
@@ -0,0 +1,40 @@
+// console.log("this is me");
+// console.warn("its u")
+// console.error("n its mohan")
+
+// variables in js
+var num1 = 46;
+var num2 = 45;
+console.log(num1 + num2)
+
+// data types in js
+var str = "its a string"  //strings can be written in both , single quote as well as double quote
+console.log(str)
+
+//stotre multiple objects
+var marks = {
+    nik : 69,
+    ritu : 70,
+    rahul : 3
+}
+console.log(marks)
+
+//booleans
+var a = true;
+var b = false;
+console.log(b,a)
+
+// var und = undefined
+var und;
+console.log(und)
+
+var n = null;
+console.log(n);
+
+//there are two types of data types in js
+//1 . primitive data types : und, null, num, string, boolean, symbol
+//2 . reference data types : arrays, objects
+
+var arr = [1,2,3,4,"ritu"]
+console.log(arr)
+console.log(arr[0]) //index starts form 0
---
+++ operators.js
@@ -0,0 +1,41 @@
+//arithmetic operators ; +,-,*, /
+var a = 33;
+var b = 56;
+console.log("The value of a + b is :",a+b)
+console.log("The value of a - b is :",a-b)
+console.log("The value of a * b is :",a*b)
+console.log("The value of a / b is :",a/b)
+
+//assignment operators 
+var c = 45
+c -= 2
+c += 2
+c /= 2
+c *= 2
+console.log(c)
+
+//comparison operator 
+var x = 33
+var y = 34
+console.log(x == y)
+console.log(x >= y)
+console.log(x <= y)
+console.log(x > y)
+console.log(x < y)
+
+//logical opetators 
+//logical and(&&)
+console.log(true && true) // in && operator if both ther statements are trues then only it will be true , else it will return false.
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+//logocal or (||) if any one is true it will return true , 
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+
+//logocal not (!)  ture will be false and false will be true
+console.log(!false)
+console.log(!true)
