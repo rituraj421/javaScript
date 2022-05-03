90cd7cff639155cbe1c37921b174fe350f9f5acd
rituraj <riturajroj1234@gmail.com>
2022-05-03 23:38:24 +0530

    master

---
+++ conditional.js
@@ -0,0 +1,14 @@
+var age = window. prompt("Enter your age : ") //window.prompt is used to take input from user
+if (age >= 18){
+    console.log("You are adult")
+}
+else{
+    console.log("you are bachha")
+}
+
+if(age = 32){
+    console.log("not a baccha")
+}
+else if(age > 56){
+    console.log("buddhe ho gae ho")
+}
---
+++ functions.js
@@ -0,0 +1,8 @@
+function avg(a,b){
+    c = (a + b)/2;
+    return c; //or else directly return (a + b)/2;
+}
+
+c1 = avg(4,6);
+c2 = avg(14,16);
+console.log(c1,c2);
