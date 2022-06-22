dedbf8ed34466b6b9a3c0966d6aff5651974d17d
rituraj <riturajroj1234@gmail.com>
2022-06-22 23:12:15 +0530

    main

--- interval.js
+++
@@ -1,7 +0,0 @@
-sum = (a,b)=>{
-    return a+b;
-}
-logKaro = ()=>{
-    console.log("I am your log")
-}
-setTimeout(logKaro,2000);
---
+++ master/ritu.js
@@ -0,0 +1,7 @@
+console.log("hey raja");
+console.log(3+4); //acts as a calculator
+console.log([2,3,4,5,6,]);
+console.table({ritu:'developer', aye: 'aye'});
+//console.clear() //it clears whole console
+console.timeEnd('your code took');
+console.assert(34>466, 'Age > 34 is not possible');
