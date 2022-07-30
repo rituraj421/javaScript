a6547b083d425a1d73896015d9aac23881d6bb29
rituraj <riturajroj1234@gmail.com>
2022-07-30 23:53:40 +0530

    js

---
+++ master/mathObj.js
@@ -0,0 +1,11 @@
+a = Math;  //will print all the math functions
+b = Math.PI;
+c = Math.E;
+d = Math.ceil(5.7);  //it will roundoff the values to upper number
+e = Math.round(5.4); //it also rounds off
+f = Math.floor(5.7); //rounds off to smaller value
+g = Math.abs(-4); // gives the absolute value
+h = Math.sqrt(4); //gives the sqr root value 
+i = Math.random(); //generatres a random number , we can also specify the range in which we want random number 
+console.log(i);
+
